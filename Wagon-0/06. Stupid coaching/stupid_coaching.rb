9523ba326d66780937040bded50dc9895f44b321
MannyV <evayleux@gmail.com>
2014-01-06 18:35:48 +0100

    Ex 6

--- Wagon-0/06. Stupid coaching/stupid_coaching.rb
+++ Wagon-0/06. Stupid coaching/stupid_coaching.rb
@@ -1,5 +1,19 @@
-def stupid_coaching 
-  # your code goes here
+def stupid_coaching
+        puts "Speak!"
+        text = gets.chomp
+
+        until text == "I am going to work right now SIR !"
+                if text.include? "?"
+                        puts "Silly question, just get dressed and go to work!"
+                else
+                        puts "I don't care son, just get dressed and go to work!"
+                end
+                puts "Speak again!"
+                text = gets.chomp
+        end
+
+        puts "OK!"
+
 end
 
 # Calling the method
