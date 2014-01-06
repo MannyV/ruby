0a1f1b111e706624e1ca5d30615c00522d590298
MannyV <evayleux@gmail.com>
2014-01-06 18:04:15 +0100

    Name

--- Wagon-0/05. What's your name ?/introduce_yourself.rb
+++ Wagon-0/05. What's your name ?/introduce_yourself.rb
@@ -1,1 +1,17 @@
 # your code goes here, introduce yourself !
+puts 'What\'s your first name?'
+first_name = gets.chomp
+
+puts 'What\'s your middle name?'
+middle_name = gets.chomp
+
+puts 'What\'s your last name?'
+last_name = gets.chomp
+
+puts "Hello, "+first_name+" "+middle_name+" "+last_name+"!"
+puts "Hello, #{first_name} #{middle_name} #{last_name} !"
+
+length = first_name.length + middle_name.length + last_name.length
+puts "You have #{length} characters in your name"
+
+puts "Merci !"  unless first_name.empty? || middle_name.empty? || last_name.empty?
