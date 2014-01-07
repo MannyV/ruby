94d3325a071837fc07e7ec21a4374b6c8c81c267
MannyV <evayleux@gmail.com>
2014-01-07 16:00:46 +0100

    1-02

--- Wagon-1/02. Sorting the wagon/wagon_sort.rb
+++ Wagon-1/02. Sorting the wagon/wagon_sort.rb
@@ -1,5 +1,17 @@
-def wagon_sort( students_array )
-  # your code to sort the array and output students
+def wagon_sort(students_array)
+   # your code to sort the array and output students
+	puts students_array.sort_by {|name| name.downcase}
 end
+
+table = Array.new
+
+while true
+	puts "Type a student name:"
+	name = gets.chomp
+	break if name == ""
+	table << name
+end
+
+wagon_sort(table)
 
 # your code to interact with the user..
