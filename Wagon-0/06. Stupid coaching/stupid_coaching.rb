18795ebe8091266a09705dce1c4e9f0b401afe82
MannyV <evayleux@gmail.com>
2014-01-07 12:23:27 +0100

    06 Solution Optim

--- Wagon-0/06. Stupid coaching/stupid_coaching.rb
+++ Wagon-0/06. Stupid coaching/stupid_coaching.rb
@@ -1,19 +1,28 @@
+def coach_merge(bla, conn)
+  t = conn ?  " " : " take the subway and " 
+  if bla.end_with? "?"
+    puts "Silly question,#{t}just get dressed and go to work!"
+  else
+    puts "I don't care son,#{t}just get dressed and go to work!"
+  end
+end
+
+def coach_response(affection, pass) 
+  answer = gets.chomp
+  puts "I can feel your motivation son!" if answer = answer.upcase
+  while true
+    break if answer == pass
+    coach_merge(answer, affection)
+    puts "Speak again!"
+    answer = gets.chomp
+  end
+end
+
 def stupid_coaching
-        puts "Speak!"
-        text = gets.chomp
-
-        until text == "I am going to work right now SIR !"
-                if text.include? "?"
-                        puts "Silly question, just get dressed and go to work!"
-                else
-                        puts "I don't care son, just get dressed and go to work!"
-                end
-                puts "Speak again!"
-                text = gets.chomp
-        end
-
-        puts "OK!"
-
+  puts "Speak!"
+  coach_response(true, "I am going to work")
+  puts "Good Boy!"
+  coach_response(false, "Leave me alone")
 end
 
 # Calling the method
