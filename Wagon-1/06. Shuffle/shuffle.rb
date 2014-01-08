4e8afe3f4e2f4f5df637870dd61315b2aade4672
MannyV <evayleux@gmail.com>
2014-01-08 12:36:14 +0100

    06

--- Wagon-1/06. Shuffle/shuffle.rb
+++ Wagon-1/06. Shuffle/shuffle.rb
@@ -1,8 +1,25 @@
-def shuffle( array )
-  # your code goes here
+
+def shuffle_method(array)
+shuffled_array = []
+n = array.count - 1
+
+	for i in 0..n
+		index = rand(n - i)
+		shuffled_array << array[index]
+		array.delete_at(index)
+	end
+
+	shuffled_array 
 end
- 
+
+
+def shuffle_method2(array)
+        array.sort_by {|x| rand }
+end
+
 sorted_array = (1..10).to_a
- 
-# Testing your shuffle algorithm
-p shuffle(sorted_array) != shuffle(sorted_array) # => true
+
+print shuffle_method(sorted_array)
+puts
+print shuffle_method2(sorted_array)
+puts
