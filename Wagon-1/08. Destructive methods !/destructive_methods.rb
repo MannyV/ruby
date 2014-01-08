252cf8ddb41ed23ac45ef19e47f57df529338cf0
MannyV <evayleux@gmail.com>
2014-01-08 16:54:36 +0100

    08

--- Wagon-1/08. Destructive methods !/destructive_methods.rb
+++ Wagon-1/08. Destructive methods !/destructive_methods.rb
@@ -1,2 +1,13 @@
 # Find an example which illustrates the difference between chosen_method and chosen_method!
 # Copy / Paste this example here !
+
+a = [ "d", "a", ["e", "c"], "b" ]
+
+def flatsort(a)
+	puts "Flatten! puis sort! :"
+	a.flatten!.sort!
+	puts a
+end
+
+2.times(flatsort(a)) 
+# le fait que la méthode bugge la deuxième fois alors qu'elle fonctionne la première montre que a a été changé !
