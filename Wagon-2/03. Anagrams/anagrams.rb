0ab007396db4c5d6f67e7d6ffd1e4ffe729ebbd7
MannyV <evayleux@gmail.com>
2014-01-10 11:20:30 +0100

    03

--- Wagon-2/03. Anagrams/anagrams.rb
+++ Wagon-2/03. Anagrams/anagrams.rb
@@ -1,17 +1,16 @@
 def anagrams?(a_string, another_string)
-  a_string.delete(' ').downcase.chars.to_a.sort == another_string.delete(' ').downcase.chars.to_a.sort 
+   a_string.delete(' ').downcase.chars.to_a.sort == another_string.delete(' ').downcase.chars.to_a.sort 
 end
 
-# def anagrams_on_steroids?( a_string, another_string )
-#   a_string.length != another_string.length ? "Ce ne sont pas des anagrammes" : do 
-#   	a_string.delete(' ').downcase.chars.to_a.select.each_with_object(Hash.new []) do |word, hash|
-
-#   		end
-#   	end
-# end
+def anagrams_on_steroids?( a_string, another_string )
+	h = Hash.new(0)
+  	a_string.delete(' ').downcase.chars.to_a.each { |l| h[l] += 1 }
+  	another_string.delete(' ').downcase.chars.to_a.each { |l| h[l] -= 1 }
+  	h.values.all? { |k| k == 0}
+end
 
 # complexity = ?
 puts anagrams? "Monica Lewinsky", "Nice silky woman" # => true 
 
 # complexity = ?
-# puts anagrams_on_steroids? "Monica Lewinsky", "Nice silky woman" # => true
+puts anagrams_on_steroids? "Monica Lewinsky", "Nice silky woman" # => true
