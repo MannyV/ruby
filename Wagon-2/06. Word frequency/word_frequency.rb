c2113bf07aa2c0ced54e68e913cb67d2d602756b
MannyV <evayleux@gmail.com>
2014-01-10 19:17:51 +0100

    06

--- Wagon-2/06. Word frequency/word_frequency.rb
+++ Wagon-2/06. Word frequency/word_frequency.rb
@@ -1,4 +1,8 @@
 # the file is yours !
+def stopwords(word)
+        words = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
+        words.split(',').include? word
+end
 
 def most_common_words(file, number)
 	h = Hash.new(0)
@@ -7,11 +11,13 @@ def most_common_words(file, number)
   			h.has_key?(word) ? h[word] += 1 : h[word] = 1
   		end
 	end
-	h1 = h.sort_by { |k,v| -v } 
-	result = h1.to_a
-	for i in 0..number-1 
-        puts result[i][0] + " has #{result[i][1]} occurrences"
+	
+	result_table = h.sort_by { |k,v| -v }.to_a
+	result_table = result_table.reject { |n| stopwords(n[0]) }
+
+	for i in 0..number-1
+        	puts "#{result_table[i][0]} has #{result_table[i][1]} occurrences"
     end
 end
 
-puts most_common_words('source-text.txt', 3) 
+most_common_words('source-text.txt', 3) 
