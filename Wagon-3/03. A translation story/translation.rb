d63144fe617404042199d203bf71370cc420dd33
MannyV <evayleux@gmail.com>
2014-01-14 14:50:09 +0100

    03

--- Wagon-3/03. A translation story/translation.rb
+++ Wagon-3/03. A translation story/translation.rb
@@ -8,7 +8,7 @@ $strings = {
      explanation: {
        en: 'This is an interesting exercise',
        fr: 'C\'est un exercice interessant',
-       de: 'Es ist eine interesante Ãbung'
+       de: 'Es ist eine interesante abung'
      },
      goodbye: {
        en: 'Goodbye',
@@ -19,13 +19,37 @@ $strings = {
  }
 }
 
-def getTranslation(a_string, a_language)
-  # your code goes here
+def getTranslation(a_string, a_language = :en )
+  h = $strings
+  lower_path = a_string
+  (a_string.count('.') + 1).times do
+    upper_title, lower_path = lower_path.split('.', 2)
+    h[upper_title.to_sym].nil? ? '' : h = h[upper_title.to_sym]
+    end
+    return h[a_language.to_sym].nil? ? h[:en] : h[a_language.to_sym]
+end
+
+def getTranslation_recursive(a_string, a_language = :en ) # NON-WORKING NEEDS CODE REVIEW !!
+  h = $strings
+  a_string.split('.').each do |path|
+    if h[path.to_sym].is_a?(Hash) 
+       h = h[path.to_sym]
+       getTranslation(path, a_language = :en)
+    else 
+       return h[a_language.to_sym].nil? ? h[:en] : h[a_language.to_sym]
+    end
+  end
 end
 
 # testing your program 
+# puts getTranslation_recursive('home.intro', 'fr')
 
-getTranslation('home.intro', 'fr') == 'Bienvenue sur Le Wagon' # => true
-getTranslation('home.intro', 'es') == 'Welcome on Le Wagon' # => true
-getTranslation('home.content.goodbye') == 'Goodbye' # => true
-getTranslation('unvalid.path','en') == '' # => true
+puts getTranslation('home.intro', 'fr') 
+puts getTranslation('home.intro', 'es')
+puts getTranslation('home.content.goodbye') 
+puts getTranslation('unvalid.path','en')
+
+# puts getTranslation_recursive('home.intro', 'fr') 
+# puts getTranslation_recursive('home.intro', 'es')
+# puts getTranslation_recursive('home.content.goodbye') 
+# puts getTranslation_recursive('unvalid.path','en')
